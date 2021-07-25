ddd6862bfda617ab9b50cc5887b46dd7de23c0ce
shubhendu-ranadive <68892098+shubhendu-ranadive@users.noreply.github.com>
2021-07-25 12:26:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
     long_description = fd.read()
 
 # replace relative urls to example files with absolute urls to the main git repo
-repo_code_url = "https://github.com/damiafuentes/DJITelloPy/tree/master"
+repo_code_url = "https://github.com/shubhendu-ranadive/DJITelloPy3/tree/master"
 long_description = long_description.replace("](examples/", "]({}/examples/".format(repo_code_url))
 
 setuptools.setup(
@@ -12,13 +12,13 @@ setuptools.setup(
     packages=['djitellopy'],
     version='2.3.1',
     license='MIT',
-    description='Tello drone library including support for video streaming, swarms, state packets and more',
+    description='Tello drone library including support for video streaming, state packets and more',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Jakob Löw',
-    author_email='djitellopy@m4gnus.de',
-    url='https://github.com/damiafuentes/DJITelloPy',
-    download_url='https://github.com/damiafuentes/DJITelloPy/archive/2.3.1.tar.gz',
+    author='Ranadive Shubhendu',
+    author_email='kazutokirigaya612@gmail.com',
+    url='https://github.com/shubhendu-ranadive/DJITelloPy3',
+    download_url='https://github.com/shubhendu-ranadive/DJITelloPy3/archive/2.3.1.tar.gz',
     keywords=['tello', 'dji', 'drone', 'sdk', 'official sdk'],
     install_requires=[
         'numpy',
